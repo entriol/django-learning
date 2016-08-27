51ea446a21f0479d8578280bfb11c0c8b15a18ee
entriol <entriol@outlook.com>
2016-08-27 20:26:09 +0300

    error (404) page

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Course
 
@@ -9,5 +9,5 @@ def course_list(request):
 
 
 def course_detail(request, pk):
-    course = Course.objects.get(pk=pk)
+    course = get_object_or_404(Course, pk=pk)
     return render(request, 'courses/course_detail.html', {'course': course})
